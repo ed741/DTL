220f379b9f6410f006ab3378c456a694961391e2
edward <es916@ic.ac.uk>
2022-03-10 20:57:31 +0000

    Initial ast

---
+++ dtl/ast.py
@@ -0,0 +1,143 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from dataclasses import dataclass
+from typing import List, Mapping, Dict
+
+
+class VSpace():
+    def __init__(self):
+        raise TypeError
+    def __str__(self) -> str:
+        return "(!!VSPACE!!)"
+
+@dataclass
+class RVSpace():
+    dim : int
+    def __str__(self) -> str:
+        return f"R{self.dim}"
+
+@dataclass
+class varIndexOBJ():
+    vspace : VSpace
+    def __str__(self) -> str:
+        return f"indexOBJ({self.vspace})"
+
+class astNode():
+    def __init__(self):
+        raise TypeError
+    def __str__(self) -> str:
+        return "(!!BASE NODE TYPE!!)"
+
+@dataclass
+class Index(astNode):
+    name : str
+    def __str__(self) -> str:
+        return self.name
+
+@dataclass
+class VarIndex(Index):
+    var: str
+    def __str__(self) -> str:
+        return self.var
+
+@dataclass
+class IntIndex(Index):
+    i: int
+    def __str__(self) -> str:
+        return str(self.i)
+
+class TensorExpr(astNode):
+    def __init__(self):
+        raise TypeError
+    def __str__(self) -> str:
+        return "(!!TENSOR EXPR NODE TYPE!!)"
+    def __getitem__(self, indices):
+        return IndexedTensor(self, indices)
+
+class ScalarExpr(astNode):
+    def __init__(self):
+        raise TypeError
+    def __str__(self) -> str:
+        return "(!!Scalar Expr NODE TYPE!!)"
+
+@dataclass
+class Literal(ScalarExpr):
+    f : float
+    def __str__(self) -> str:
+        return str(self.f)
+
+@dataclass
+class IndexedTensor(ScalarExpr):
+    tensor: TensorExpr
+    indices: List[Index]
+    def __str__(self) -> str:
+        return f"{self.tensor}[{','.join(map(str, self.indices))}]"
+
+class BinOp(ScalarExpr):
+    lhs : IndexedTensor
+    rhs : IndexedTensor
+    def __str__(self) -> str:
+        return f"{self.lhs}(!!BINOP!!){self.rhs}"
+
+@dataclass
+class MulBinOp(BinOp):
+    def __str__(self) -> str:
+        return f"{self.lhs} * {self.rhs}"
+
+class UnOp(ScalarExpr):
+    sub : ScalarExpr
+    def __str__(self) -> str:
+        return f"(!!UNOP!!)({self.sub.__str__()})"
+
+@dataclass
+class AbsUnOp(UnOp):
+    def __str__(self) -> str:
+        return f"abs({self.sub})"
+
+@dataclass
+class indexSum(ScalarExpr):
+    sub : ScalarExpr
+    indices : List[Index]
+    def __str__(self) -> str:
+        return f"Sum{self.indices}){self.sub})"
+
+@dataclass
+class TensorVariable(TensorExpr):
+    name : str
+    def __str__(self) -> str:
+        return self.name
+
+@dataclass
+class deIndex(TensorExpr):
+    tensor : ScalarExpr
+    indices : List[Index]
+    def __str__(self) -> str:
+        return f"({self.tensor})|{','.join(map(str, self.indices))}|"
+
+@dataclass()
+class Lambda(astNode):
+    vars : List[TensorVariable]
+    sub : astNode
+    def __str__(self) -> str:
+        return f"{{{type(self).__name__}: {self.vars}:{self.sub}}}"
+
+
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    i = Index("i")
+    j = Index("j")
+    A = Lambda([A:=TensorVariable("A")], deIndex(A[j, i], [i, j]))
+    print(str([j, i]))
+    print(str(A))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/

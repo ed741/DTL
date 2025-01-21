45b266a2fbc53951ed049b79b438e84c9678f5d9
edward <edward@edstow.co.uk>
2025-01-21 13:16:21 +0000

    update CountReification.py

--- evaluation/experiments/CountReification.py
+++ evaluation/experiments/CountReification.py
@@ -1,4 +1,5 @@
 import csv
+import os
 import sys
 
 from xdsl.dialects import builtin
@@ -8,18 +9,30 @@ from xdsl.transforms.experimental.dlt.layout_graph import LayoutGraph
 
 results = {}
 table_path = f"layouts_table.csv"
-with open(table_path, "r") as f:
-    csv_reader = csv.reader(f, delimiter=",")
-    csv_reader.__next__()
-    for row in csv_reader:
-        results[int(row[0])] = int(row[1])
-print(f"Read table from {table_path}")
+
+if os.path.exists(table_path):
+    with open(table_path, "r") as f:
+        csv_reader = csv.reader(f, delimiter=",")
+        csv_reader.__next__()
+        for row in csv_reader:
+            results[int(row[0])] = int(row[1])
+    print(f"Read table from {table_path}")
+else:
+    with open(table_path, "w+") as f:
+        csv_writer = csv.writer(f, delimiter=",")
+        csv_writer.writerow(
+            [
+                "n",
+                "layouts",
+            ]
+        )
+    print(f"Written table to {table_path}")
 
 reify_config = ReifyConfig(
     dense=True,
     unpacked_coo_buffer_options=frozenset([0]),
-    separated_coo_buffer_options=frozenset([0]),
-    separated_coo_buffer_index_options=frozenset([builtin.i32]),
+    separated_coo_buffer_options=frozenset([]),
+    separated_coo_buffer_index_options=frozenset([]),
     coo_minimum_dims=1,
     arith_replace=False,
     force_arith_replace_immediate_use=True,
@@ -28,39 +41,31 @@ reify_config = ReifyConfig(
 )
 previous_generated_layouts = {}
 
-for n in range(int(sys.argv[1])):
-    if n in results:
-        continue
-    dims = [dlt.DimensionAttr(str(i+1),i+1) for i in range(n)]
-    dlt_type = dlt.TypeType([([],dims,builtin.f32)])
-    ptr_type = dlt.PtrType(dlt_type, base=True, identity=f"test_{n}")
-    allocate = dlt.AllocOp(ptr_type, {},[])
-    layout_graph = LayoutGraph()
-    layout_graph.add_ssa_value(allocate.res)
+with open(table_path, "a") as f:
+    csv_writer = csv.writer(f, delimiter=",")
+    for n in range(int(sys.argv[1])):
+        if n in results:
+            continue
+        dims = [dlt.DimensionAttr(str(i+1),i+1) for i in range(n)]
+        dlt_type = dlt.TypeType([([],dims,builtin.f32)])
+        ptr_type = dlt.PtrType(dlt_type, base=True, identity=f"test_{n}")
+        allocate = dlt.AllocOp(ptr_type, {},[])
+        layout_graph = LayoutGraph()
+        layout_graph.add_ssa_value(allocate.res)
 
 
-    generator = LayoutGenerator(layout_graph, {ptr_type.identification:reify_config})
-    generator._generated_layouts = previous_generated_layouts
-    layouts = generator.generate_mappings()
-    print(f" for n = {n}: |layouts| = {len(layouts)}")
-    results[n] = len(layouts)
+        generator = LayoutGenerator(layout_graph, {ptr_type.identification:reify_config})
+        generator._generated_layouts = previous_generated_layouts
+        layouts = generator.generate_mappings()
+        print(f" for n = {n}: |layouts| = {len(layouts)}")
+        results[n] = len(layouts)
+        csv_writer.writerow(
+            [
+                str(n),
+                str(len(layouts)),
+            ]
+        )
 
 
 print(results)
 
-with open(table_path, "w") as f:
-    csv_writer = csv.writer(f, delimiter=",")
-    csv_writer.writerow(
-        [
-            "n",
-            "layouts",
-        ]
-    )
-
-    for n, layouts in results:
-        csv_writer.writerow(
-            [
-                str(n), str(layouts)
-            ]
-        )
-print(f"Written table to {table_path}")
